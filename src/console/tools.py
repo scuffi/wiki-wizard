f0c837ae2b5b0b763be80ff7a7550742a7649e07
Scuffi <grawlixpvp@gmail.com>
2023-11-19 23:36:26 +0000

    Added monitor tool to allow easier task management

---
+++ src/console/tools.py
@@ -0,0 +1,15 @@
+from rich.console import Console
+
+console = Console()
+
+
+def monitor(message: str):
+    def inner_function(func):
+        def wrapper(*args, **kwargs):
+            with console.status(message):
+                result = func(*args, **kwargs)
+            return result
+
+        return wrapper
+
+    return inner_function
--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 from dotenv import load_dotenv
+from tasks import headings
 
 load_dotenv()
+
+
+# * Project start
+print(headings.generate_headings("Toothpaste").content)
