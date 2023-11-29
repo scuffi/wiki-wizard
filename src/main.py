e98a242e54d325e17e32e9faaf47af09016836a2
Scuffi <grawlixpvp@gmail.com>
2023-11-29 23:50:32 +0000

    Complete micro-e2e

--- src/main.py
+++ src/main.py
@@ -3,31 +3,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-from pathlib import Path
 from rich import print
 
-from tasks import notion, headings, parsing
-
-
-def write(section, title):
-    p = Path("./generated/")
-    p.mkdir(parents=True, exist_ok=True)
-    fn = f"{title}.md"
-    filepath = p / fn
-    with filepath.open("w", encoding="utf-8") as f:
-        f.write(section.dump())
+from tasks import notion, headings, parsing, writing
 
 
 # * Project start
+title = "Toothpaste"
+
 database_id = notion.split_url(
     "https://www.notion.so/archief/93992d8440fa4111b06e7cc5748fac5e?v=6429bb958ce6452497c5089c15e9e6f2"
 )
-primary_page = notion.create_primary_page(
-    database_id, title="Python", category="Programming"
-)
-# console = Console()
 
-title = "Toothpaste"
+primary_page = notion.create_primary_page(
+    database_id, title=title, category="Programming"
+)
 
 sections = headings.generate_headings(title)
 
@@ -45,17 +35,14 @@ for section in sections:
 
         notion.write_to_page(page, content)
 
-        # written_section = writing.write_section(
-        #     section=section, heading=heading, title=title
-        # )
-        # heading.content = written_section
-        # parsed = parsing.parse2notion(written_section)
-
         if node.is_leaf():
-            # TODO: Create a page for this section
-            # TODO: Write content to page
-            ...
-        break
+            written_section = writing.write_section(
+                section=section, heading=heading, title=title
+            )
+            heading.content = written_section
+            parsed = parsing.parse2notion(written_section)
+
+            notion.create_subpage(page, heading.title, content=parsed)
 
     # write(section, title)
     break
--- src/tasks/notion.py
+++ src/tasks/notion.py
@@ -69,5 +69,51 @@ def create_primary_page(database: str, title: str, category: str):
     return notion.pages.create(**payload)["id"]
 
 
-def create_subpage():
-    ...
+def create_subpage(primary_page: str, title: str, content: list | None):
+    payload = {
+        # "cover": {
+        #     "type": "external",
+        #     "external": {
+        #         "url": "https://upload.wikimedia.org/wikipedia/commons/6/62/Tuscankale.jpg"
+        #     },
+        # },
+        "icon": {
+            "type": "emoji",
+            "emoji": "🥬",
+        },
+        "parent": {
+            "type": "page_id",
+            "page_id": primary_page,
+        },
+        "properties": {
+            "title": {"title": [{"text": {"content": title}}]},
+        },
+        # "children": [
+        #     {
+        #         "object": "block",
+        #         "heading_2": {"rich_text": [{"text": {"content": "Lacinato kale"}}]},
+        #     },
+        #     {
+        #         "object": "block",
+        #         "paragraph": {
+        #             "rich_text": [
+        #                 {
+        #                     "text": {
+        #                         "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+        #                         "link": {
+        #                             "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
+        #                         },
+        #                     },
+        #                     "href": "https://en.wikipedia.org/wiki/Lacinato_kale",
+        #                 }
+        #             ],
+        #             "color": "default",
+        #         },
+        #     },
+        # ],
+    }
+
+    if content:
+        payload["children"] = content
+
+    return notion.pages.create(**payload)["id"]
