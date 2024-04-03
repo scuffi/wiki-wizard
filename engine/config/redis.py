dfed5d7d78b815f28854dad52cc3ebc9451cc595
Scuffi <grawlixpvp@gmail.com>
2024-04-03 23:48:19 +0100

    Updated status messages

--- engine/config/redis.py
+++ engine/config/redis.py
@@ -7,12 +7,12 @@ pool = BlockingConnectionPool(host="redis", port=6379, decode_responses=True)
 redis_client = StrictRedis(connection_pool=pool)
 
 class Status(Enum):
-    WAITING = "waiting"
+    WAITING = "Waiting"
     PAGE_SETUP = "Setting up page"
     GENERATING_SECTIONS = "Generating sections"
     GENERATING_CONTENT = "Generating content"
-    COMPLETE = "complete"
-    FAILED = "failed"
+    COMPLETE = "Completed"
+    FAILED = "Failed"
 
 def update_status(page_id: str, status: Status):
     redis_client.hset(f"generation:{page_id}", "status", status.value if isinstance(status, Status) else status)
